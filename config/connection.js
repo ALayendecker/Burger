dd34381ca6b598a88fdcadf7cb83e45f1757e06c
ALayendecker <alcommits@gmail.com>
2019-10-28 13:04:58 -0500

    fixing hopefully heroku deployment

--- config/connection.js
+++ config/connection.js
@@ -1,20 +1,20 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
 var connection;
+require("dotenv").config();
+var keys = require("../keys");
+var connection = mysql.createConnection(keys);
 
 if (process.env.JAWSDB_URL) {
   connecton = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
-  connection = mysql = createConnection({
+  connection = mysql.createConnection({
     host: "localhost",
     user: "root",
     password: process.env.MYSQLKEYS,
     database: "burgers_db"
   });
 }
-require("dotenv").config();
-var keys = require("../keys");
-var connection = mysql.createConnection(keys);
 
 // Make connection.
 connection.connect(function(err) {
